933ca5f34c44950624a759cda66f7656e0465d77
Michael Poirier <mikepoirier26@gmail.com>
2022-03-09 19:25:15 -0700

    Fix player repo

--- src/repository/player.rs
+++ src/repository/player.rs
@@ -4,7 +4,6 @@ use crate::domain::player::Player;
 
 pub trait PlayerRepository {
     fn save(&self, player: Player) -> Player;
-    fn get_all(&self) -> Vec<Player>;
     fn get(&self, id: &str) -> Option<Player>;
     fn delete(&self, id: &str) -> Option<Player>;
 }
@@ -24,18 +23,17 @@ impl InMemoryPlayerRepository {
 impl PlayerRepository for InMemoryPlayerRepository {
     fn save(&self, player: Player) -> Player {
         let mut data = self.data.lock().unwrap();
-        data.insert(player.id(), player);
+        data.insert(player.id(), player.clone());
         player
-    }
-
-    fn get_all(&self) -> Vec<Player> {
-        let data = self.data.lock().unwrap();
-        data.into_values().collect()
     }
 
     fn get(&self, id: &str) -> Option<Player> {
         let data = self.data.lock().unwrap();
-        data.get(id)
+        if data.contains_key(id) {
+            Some(data[id].clone())
+        } else {
+            None
+        }
     }
 
     fn delete(&self, id: &str) -> Option<Player> {
@@ -48,34 +46,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_should_return_an_empty_vec_when_empty() {
-        let expected = vec![];
-        let repo = InMemoryPlayerRepository::new();
-
-        let actual = repo.get_all();
-
-        assert_eq!(actual, expected)
-    }
-
-    #[test]
     fn it_should_return_player_on_save() {
         let player = Player::new("id", "username");
         let expected = player.clone();
         let repo = InMemoryPlayerRepository::new();
 
         let actual = repo.save(player);
-
-        assert_eq!(actual, expected);
-    }
-
-    #[test]
-    fn it_can_add_a_player() {
-        let player = Player::new("id", "username");
-        let expected = vec![player.clone()];
-        let repo = InMemoryPlayerRepository::new();
-
-        repo.save(player);
-        let actual = repo.get_all();
 
         assert_eq!(actual, expected);
     }
